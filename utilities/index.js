9328850f8cc9cf07ccafb8e660c4dba5f50efaef
DC-CC-21 <handstands1000@gmail.com>
2024-06-24 14:18:40 -0400

    added error handling

--- utilities/index.js
+++ utilities/index.js
@@ -1,6 +1,14 @@
 const invModel = require("../models/inventory-model");
 const Util = {};
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
 /**
  * Retrieves all classifications and returns an HTML unordered list of links to view the inventory by classification.
  * @returns {Promise<string>} A string representing an HTML unordered list of classification links.
@@ -28,7 +36,6 @@ Util.getNav = async function () {
   return list;
 };
 
-
 /**
  * Builds a grid of vehicles based on the provided data.
  * If data is empty, a message is returned indicating that no matching vehicles could be found.
@@ -36,40 +43,48 @@ Util.getNav = async function () {
  * @returns {string} - A string representing an HTML unordered list of the vehicles.
  */
 Util.buildClassificationGrid = async function (data) {
-    let grid;
-    if (data.length > 0) {
-        grid = '<ul id="inv-display">'; // Start the grid with an unordered list element
-        data.forEach(vehicle => {
-            grid += `
+  let grid;
+  if (data.length > 0) {
+    grid = '<ul id="inv-display">'; // Start the grid with an unordered list element
+    data.forEach((vehicle) => {
+      grid += `
                 <li>
                     <a
                         href="../../inv/detail/${vehicle.inv_id}"
-                        title="View ${vehicle.inv_make} ${vehicle.inv_model} details"
+                        title="View ${vehicle.inv_make} ${
+        vehicle.inv_model
+      } details"
                         >
-                        <img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.make} ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors">
+                        <img src="${vehicle.inv_thumbnail}" alt="Image of ${
+        vehicle.make
+      } ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors">
                     </a>
                     <div class="namePrice"> <!-- Start a div to contain the vehicle's name and price -->
                         <hr/>
                         <h2>
                             <a
                                 href="../../inv/detail/${vehicle.inv_id}"
-                                title="View ${vehicle.inv_make} ${vehicle.inv_model} details"
+                                title="View ${vehicle.inv_make} ${
+        vehicle.inv_model
+      } details"
                                 >
                                 ${vehicle.inv_make} ${vehicle.inv_model}
                             </a>
                         </h2>
                         <span> <!-- Start a span to contain the vehicle's price -->
-                            $${new Intl.NumberFormat("en-US").format(vehicle.inv_price)}
+                            $${new Intl.NumberFormat("en-US").format(
+                              vehicle.inv_price
+                            )}
                         </span>
                     </div>
                 </li>
-            `
-        })
-        grid += "</ul>"; // End the grid with the closing unordered list element
-    } else {
-        grid = "<p>Sorry, no matching vehicles could be found.</p>"; // If there are no vehicles, return a message indicating that no matching vehicles could be found
-    }
-    return grid;
-}
+            `;
+    });
+    grid += "</ul>"; // End the grid with the closing unordered list element
+  } else {
+    grid = "<p>Sorry, no matching vehicles could be found.</p>"; // If there are no vehicles, return a message indicating that no matching vehicles could be found
+  }
+  return grid;
+};
 
 module.exports = Util;
